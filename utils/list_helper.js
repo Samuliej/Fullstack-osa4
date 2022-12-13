938dea191b74d5e747a05db4bb1ee9de155734ae
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2022-12-13 20:33:29 +0200

    4.5*: apufunktioita ja yksikkötestejä step3 complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,14 +3,30 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    console.log(blogs)
-    console.log(blogs[0].likes)
     const reducer = (sum, blog) =>  {
         return sum + blog.likes
     }
     return blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    let mostLikes = blogs[0].likes
+    let favBlog = blogs[0]
+    blogs.forEach(blog => {
+        if (blog.likes > mostLikes) {
+            mostLikes = blog.likes
+            favBlog = blog
+        }
+    })
+
+    return {
+        title: favBlog.title,
+        author: favBlog.author,
+        likes: mostLikes
+    }
+    
+}
   
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
