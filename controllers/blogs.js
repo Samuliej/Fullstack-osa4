d03be1d8122a4773b2ec488e7d0809bca2e4c0b2
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2023-05-28 13:44:04 +0300

    4.19: blogilistan laajennus step7 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+// eristää tokenin headerista authorization
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -18,8 +29,13 @@ blogsRouter.get('/:id', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  // Varmistetaan tokenin oikeellisuus
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 
-  const user = await User.findById(body.user)
+  const user = await User.findById(decodedToken.id)
 
   console.log(body)
   let blog = new Blog()
